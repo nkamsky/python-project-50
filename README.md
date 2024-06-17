c59504849d1d34c013a593b79f44b61df49dd3e1
Dmitry Krasnov <atlanticl@yandex.ru>
2024-06-17 20:12:23 +0300

    step 4

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/nkamsky/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/nkamsky/python-project-50/actions)
 
+<a href="https://codeclimate.com/github/nkamsky/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/c2ae0d14002f261beb02/maintainability" /></a>
+
+<a href="https://codeclimate.com/github/nkamsky/python-project-50/test_coverage"><img src="https://api.codeclimate.com/v1/badges/c2ae0d14002f261beb02/test_coverage" /></a>
+
 Пример:
 
 [![asciicast](https://asciinema.org/a/80DRUN8IwwmtX1i0f5dTznHq8.svg)](https://asciinema.org/a/80DRUN8IwwmtX1i0f5dTznHq8)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,8 +5,8 @@ import json
 
 
 def get_files():
-    parser = argparse.ArgumentParser(description=
-                                     'Compares two configuration files\
+    parser = argparse.ArgumentParser(description='\
+                                    Compares two configuration files\
                                     and shows a difference.')
     parser.add_argument('first_file', help='first_file')
     parser.add_argument('second_file', help='second_file')
@@ -17,9 +17,9 @@ def get_files():
 
 def check_bool(data):
     for key, value in data.items():
-        if value == True:
+        if value is True:
             data[key] = 'true'
-        elif value == False:
+        elif value is False:
             data[key] = 'false'
 
 
@@ -34,17 +34,17 @@ def generate_diff(file1, file2):
     result = ''
     for key in sorted(all_keys):
         if key in data1 and key not in data2:
-            result += f' - {key}: {data1[key]}\n'
+            result += f'  - {key}: {data1[key]}\n'
         elif key not in data1 and key in data2:
-            result += f' + {key}: {data2[key]}\n'
+            result += f'  + {key}: {data2[key]}\n'
         else:
             if data1[key] != data2[key]:
-                result += f' - {key}: {data1[key]}\n'
-                result += f' + {key}: {data2[key]}\n'
+                result += f'  - {key}: {data1[key]}\n'
+                result += f'  + {key}: {data2[key]}\n'
             else:
-                result += f'   {key}: {data2[key]}\n'
-    print('{\n' + result + '}')
-    # return result
+                result += f'    {key}: {data2[key]}\n'
+    # print('{\n' + result + '}')
+    return '{\n' + result + '}'
 
 
 def main():
